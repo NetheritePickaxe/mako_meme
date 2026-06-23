d49974abdef2846efa8fd9fc1e53f52d123c70c1
NetheritePickaxe <gsc8379@163.com>
2026-06-23 13:40:03 +0800

    chore: disable GitHub Pages deploy and update compileSdk to 34

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.sticker.sticker_manager"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
